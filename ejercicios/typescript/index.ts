ee1d1b71e86b682671299aece9d2709d9adc6bd3
arq-gabo <jgabrielguerral@gmail.com>
2021-01-31 17:05:25 -0500

    Tipos Básicos

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,39 @@
-console.log("Hello TypeScript")
+//Boolean
+let muted: boolean = true;
+muted = false;
 
-function add(a: number, b: number){
-    return a + b
+// numbers
+let numerador: number = 42
+let denominador: number = 6
+let resultado = numerador / denominador
+
+// String
+let nombre: string = "Aarón"
+let saludo = `Te extraño ${nombre}`
+
+// Arreglos
+let people: string[] = []
+people = ["Aarón", "Maria", "Ricardo"]
+people.push("9000")
+
+let peopleAndNumbers: Array< string | number > = []
+peopleAndNumbers.push("Aarón")
+peopleAndNumbers.push("2014")
+
+// Enum
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+    Azul = "Azul",
+    Amarillo = "Amarillo"
 }
 
-const sum = add(2, 3)
+let colorFavorito: Color = Color.Rojo
+console.log(`Mi Color Favorito es ${colorFavorito}`)
+
+// Any
+let comodin: any = "Joker"
+comodin = { type: "WildCard" }
+
+// Object
+let someObject: object = { type: "WildCard" }
