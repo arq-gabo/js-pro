a3f411eefd4fe559d1eeecb4124970f64ffbdab8
arq-gabo <jgabrielguerral@gmail.com>
2021-01-31 22:02:07 -0500

    Funciones

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,39 +1,22 @@
-//Boolean
-let muted: boolean = true;
-muted = false;
-
-// numbers
-let numerador: number = 42
-let denominador: number = 6
-let resultado = numerador / denominador
-
-// String
-let nombre: string = "Aarón"
-let saludo = `Te extraño ${nombre}`
-
-// Arreglos
-let people: string[] = []
-people = ["Aarón", "Maria", "Ricardo"]
-people.push("9000")
-
-let peopleAndNumbers: Array< string | number > = []
-peopleAndNumbers.push("Aarón")
-peopleAndNumbers.push("2014")
-
-// Enum
-enum Color {
-    Rojo = "Rojo",
-    Verde = "Verde",
-    Azul = "Azul",
-    Amarillo = "Amarillo"
+function add(a: number, b: number): number{
+    return a + b
 }
 
-let colorFavorito: Color = Color.Rojo
-console.log(`Mi Color Favorito es ${colorFavorito}`)
+const sum = add(4, 6);
 
-// Any
-let comodin: any = "Joker"
-comodin = { type: "WildCard" }
+function createAddler(a: number): (number) => number {
+    return function(b: number){
+        return b + a
+    }
+}
 
-// Object
-let someObject: object = { type: "WildCard" }
+const addFour = createAddler(4)
+const fourPlusSix = addFour(6)
+
+function fullName(firstName: string, lastName: string = "Guerra"): string {
+    return `${firstName} ${lastName}`
+}
+
+const aaroncito = fullName("Aaron")
+
+console.log(aaroncito)
