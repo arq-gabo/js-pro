c8932825b98cffb1ba3c6630b1179cbebbbf960e
arq-gabo <jgabrielguerral@gmail.com>
2021-01-31 23:13:56 -0500

    Convertir el proyecto a TypeScript

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,6 @@
-import MediaPlayer from "./MediaPlayer.js";
-import AutoPlay from "./plugins/AutoPlay.js";
-import AutoPause from "./plugins/AutoPause.js";
+import MediaPlayer from "./MediaPlayer";
+import AutoPlay from "./plugins/AutoPlay";
+import AutoPause from "./plugins/AutoPause";
 
 const video = document.querySelector("video");
 const player = new MediaPlayer({
@@ -8,10 +8,10 @@ const player = new MediaPlayer({
 	plugins: [new AutoPlay(), new AutoPause()]
 });
 
-const playbutton = document.getElementById("playbutton");
+const playbutton: HTMLElement = document.getElementById("playbutton");
 playbutton.onclick = () => player.togglePlay();
 
-const mutebutton = document.getElementById("mutebutton");
+const mutebutton: HTMLElement = document.getElementById("mutebutton");
 mutebutton.onclick = () => player.toggleMute();
 
 if ("serviceWorker" in navigator) {
